618c614b88fff7e1eb05f16b5ff463afa7eef932
Elendan <arthur.despeaux@hotmail.fr>
2017-10-02 15:01:04 +0200

    fix NRE on saveall, probably causing tremendous lags

--- OpenNos.GameObject/Networking/ServerManager.cs
+++ OpenNos.GameObject/Networking/ServerManager.cs
@@ -1471,8 +1471,8 @@ namespace OpenNos.GameObject
 
         public void SaveAll()
         {
-            List<ClientSession> sessions = Sessions.Where(c => c.IsConnected).ToList();
-            sessions.ForEach(s => s.Character?.Save());
+            List<ClientSession> sessions = Sessions?.Where(c => c.IsConnected).ToList();
+            sessions?.ForEach(s => s.Character?.Save());
             DAOFactory.BazaarItemDAO.RemoveOutDated();
         }
 
