f5885637707f0378328883a8be646d4bf10689f7
Kraken <kraken.kk1@gmail.com>
2017-10-03 18:55:18 +0200

    add Monster buff --> bonus

--- OpenNos.GameObject/Map/MapMonster.cs
+++ OpenNos.GameObject/Map/MapMonster.cs
@@ -190,6 +190,7 @@ namespace OpenNos.GameObject
             _random = new Random(MapMonsterId);
             _movetime = ServerManager.Instance.RandomNumber(400, 3200);
             Buff = new ConcurrentBag<Buff>();
+            Bonus = new Bonus();
             SkillBcards = new ConcurrentBag<BCard>();
         }
 
@@ -1385,7 +1386,14 @@ namespace OpenNos.GameObject
             {
                 return;
             }
-            Buff = Buff.Where(s => !s.Card.CardId.Equals(indicator.Card.CardId));
+            if (Buff.Any(b => b.Card.CardId == indicator.Card.CardId))
+            {
+                Buff = Buff.Where(s => !s.Card.CardId.Equals(indicator.Card.CardId));
+            }
+            else
+            {
+                indicator.Card.BCards.ForEach(b => b.ApplyBonus(this));
+            }
             indicator.RemainingTime = indicator.Card.Duration;
             indicator.Start = DateTime.Now;
             Buff.Add(indicator);
@@ -1411,6 +1419,7 @@ namespace OpenNos.GameObject
             if (Buff.Contains(indicator))
             {
                 Buff = Buff.Where(s => s.Card.CardId != id);
+                indicator.Card.BCards.ForEach(b => b.RemoveBonus(this));
             }
         }
 
@@ -1425,6 +1434,14 @@ namespace OpenNos.GameObject
         {
             int value1 = 0;
             int value2 = 0;
+
+            value1 += Bonus.Number[(int)type, subtype, 0, 0];
+            value1 += Monster.Level / Bonus.Number[(int)type, subtype, 0, 1];
+            value1 += Monster.Level * Bonus.Number[(int)type, subtype, 0, 2];
+
+            value2 += Bonus.Number[(int)type, subtype, 1, 0];
+            value2 += Monster.Level / Bonus.Number[(int)type, subtype, 1, 1];
+            value2 += Monster.Level * Bonus.Number[(int)type, subtype, 1, 2];
 
             foreach (BCard entry in SkillBcards.Where(s => s != null && s.Type.Equals((byte)type) && s.SubType.Equals(subtype)))
             {
@@ -1446,7 +1463,7 @@ namespace OpenNos.GameObject
                 value2 += entry.SecondData;
             }
 
-            foreach (Buff buff in Buff)
+            /*foreach (Buff buff in Buff)
             {
                 foreach (BCard entry in buff.Card.BCards.Where(s =>
                     s.Type.Equals((byte)type) && s.SubType.Equals(subtype) &&
@@ -1469,7 +1486,7 @@ namespace OpenNos.GameObject
                     }
                     value2 += entry.SecondData;
                 }
-            }
+            }*/
 
             return new[] { value1, value2 };
         }
