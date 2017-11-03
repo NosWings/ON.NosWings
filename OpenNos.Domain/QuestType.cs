a053838ea463b7901c5cde60ce80599dab841ece
Kraken <kraken.kk1@gmail.com>
2017-11-03 12:54:30 +0100

    Add QuestType

---
+++ OpenNos.Domain/QuestType.cs
@@ -0,0 +1,32 @@
+﻿namespace OpenNos.Domain
+{
+    public enum QuestType : byte
+    {
+        Hunt = 1,
+        Collect1 = 2,
+        Collect2 = 3,
+        Brings = 4,
+        Capture1 = 5,
+        Capture2 = 6,
+        TimesSpace = 7,
+        Make = 8,
+        NumberOfKill = 9,
+        TargetReput = 10,
+        TsPoint = 11,
+        Dialog1 = 12,
+        Collect3 = 13,
+        YouNeed = 14,
+        Wear = 15,
+        Needed = 16,
+        Collect4 = 17,
+        TransmitGold = 18,
+        GoTo = 19,
+        Collect5 = 20,
+        Use = 21,
+        Dialog2 = 22,
+        UnKnow = 23,
+        Inspect = 24,
+        WinRaid = 25,
+        FlowerQuest = 26,
+    }
+}
