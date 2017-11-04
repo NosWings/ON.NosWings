303a3005c72c07f4aa6ea175063d89308b612b3a
Kraken <kraken.kk1@gmail.com>
2017-11-04 21:41:45 +0100

    Add Aphrodite76

---
+++ OpenNos.DAL.EF/Migrations/201711042039369_Aphrodite76.cs
@@ -0,0 +1,18 @@
+namespace OpenNos.DAL.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Aphrodite76 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Quest", "InfoId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Quest", "InfoId");
+        }
+    }
+}
