ae8e520ae4c83ff0745e078296a5f9d5a8c36d96
Kraken <kraken.kk1@gmail.com>
2017-11-12 00:59:47 +0100

    Add RaidDesignType

---
+++ OpenNos.Domain/RaidDesignType.cs
@@ -0,0 +1,35 @@
+﻿namespace OpenNos.Domain
+{
+    public enum RaidDesignType : byte
+    {
+        Cuby = 0,
+        Ginseng = 1,
+        Castra = 2,
+        GiantBlackSpider = 3,
+        Slade = 4,
+        ChickenKing = 5,
+        Namaju = 6,
+        GiantGrasslin = 7,
+        SnowMan = 8,
+        DesertRobberBand = 9,
+        JackOLantern = 10,
+        ChickenQueen = 11,
+        PeteOPeng = 12,
+        Kertos = 13,
+        Valakus = 14,
+        Grenigas = 15,
+        Draco = 16,
+        Glacerus = 17,
+        Foxy = 18,
+        Maru = 19,
+        Laurena = 20,
+        HongbiAndCheongbi = 21,
+        LolaLopears = 22,
+        Zenas = 23,
+        Erenia = 24,
+        Fernon = 25,
+        Fafnir = 26,
+        Yetirand = 27
+    }
+}
+
