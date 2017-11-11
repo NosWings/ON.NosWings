1816ad6f6b8310b86831e9b1def9415a5fa50e68
Kraken <kraken.kk1@gmail.com>
2017-11-11 23:55:31 +0100

    Add Aphrodite81

--- OpenNos.DAL.EF/Entities/QuestReward.cs
+++ OpenNos.DAL.EF/Entities/QuestReward.cs
@@ -16,6 +16,12 @@ namespace OpenNos.DAL.EF
 
         public int Data { get; set; }
 
+        public byte Design { get; set; }
+
+        public byte Rarity { get; set; }
+
+        public byte Upgrade { get; set; }
+
         public int Amount { get; set; }
 
         public long QuestId { get; set; }
--- OpenNos.Data/QuestRewardDTO.cs
+++ OpenNos.Data/QuestRewardDTO.cs
@@ -17,6 +17,12 @@ namespace OpenNos.Data
 
         public int Data { get; set; }
 
+        public byte Design { get; set; }
+
+        public byte Rarity { get; set; }
+
+        public byte Upgrade { get; set; }
+
         public int Amount { get; set; }
 
         public long QuestId { get; set; }
