0a8bd0ca53c45f84cdc027d76e1f20a2ceb38a65
Kraken <kraken.kk1@gmail.com>
2017-11-19 13:15:02 +0100

    Add migration

--- srcs/OpenNos.DAL.EF/Entities/Quest.cs
+++ srcs/OpenNos.DAL.EF/Entities/Quest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OpenNos.DAL.EF
 {
@@ -10,6 +7,8 @@ namespace OpenNos.DAL.EF
     {
         #region Properties
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public long QuestId { get; set; }
 
         public int QuestType { get; set; }
