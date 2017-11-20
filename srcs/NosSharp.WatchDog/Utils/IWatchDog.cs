59ce2c7de57be66121d2415618d63611603784fa
Blowa <blowaxd693@gmail.com>
2017-11-20 21:58:18 +0100

    add basic watchdog class

---
+++ srcs/NosSharp.WatchDog/Utils/IWatchDog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NosSharp.WatchDog.Utils
+{
+    interface IWatchDog
+    {
+        void RegisterNewProcess(Process process);
+
+        void RemoveProcessById(long id);
+        void Restart(long id);
+
+        void Restart(Type type);
+    }
+}
