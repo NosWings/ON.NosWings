f42b7879f2c30e85776250b28d400797b019d861
Blowa <blowaxd693@gmail.com>
2017-11-27 21:30:01 +0100

    begin brand new Master

--- srcs/NosSharp.Master/Controllers/StatController.cs
+++ srcs/NosSharp.Master/Controllers/StatController.cs
@@ -13,20 +13,20 @@ namespace OpenNos.Master.Server
     public class StatController : ApiController
     {
         // GET /stat
-        public Dictionary<int, List<AccountConnection.CharacterSession>> Get()
+        public Dictionary<int, List<AccountSession.CharacterSession>> Get()
         {
             try
             {
                 string tmp = CommunicationServiceClient.Instance.RetrieveServerStatistics();
                 Logger.Log.Info($"[WEBAPI] Stats : {tmp}");
-                Dictionary<int, List<AccountConnection.CharacterSession>> newDictionary = JsonConvert.DeserializeObject<Dictionary<int, List<AccountConnection.CharacterSession>>>(tmp);
+                Dictionary<int, List<AccountSession.CharacterSession>> newDictionary = JsonConvert.DeserializeObject<Dictionary<int, List<AccountSession.CharacterSession>>>(tmp);
 
                 return newDictionary;
             }
             catch (Exception e)
             {
                 Logger.Log.Error("[WEBAPI]", e);
-                return new Dictionary<int, List<AccountConnection.CharacterSession>>();
+                return new Dictionary<int, List<AccountSession.CharacterSession>>();
             }
         }
     }
