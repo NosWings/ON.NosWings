2bcd06fe8a1a508cfc7ab8bbc8375355bd894ad8
Elendan <arthur.despeaux@hotmail.fr>
2017-12-04 10:06:19 +0100

    continue pet skills (need to find how to trigger u_pet packet)

---
+++ srcs/OpenNos.GameObject/Packets/ClientPackets/UpetPacket.cs
@@ -0,0 +1,26 @@
+﻿using OpenNos.Core;
+using OpenNos.Domain;
+
+namespace OpenNos.GameObject
+{
+    [PacketHeader("u_pet")]
+    public class UpetPacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public int MateTransportId { get; set; }
+
+        [PacketIndex(1)]
+        public UserType TargetType { get; set; }
+
+        [PacketIndex(2)]
+        public int TargetId { get; set; }
+
+        [PacketIndex(3)]
+        public int Unknown2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{MateTransportId} {TargetType} {TargetId} 0";
+        }
+    }
+}
