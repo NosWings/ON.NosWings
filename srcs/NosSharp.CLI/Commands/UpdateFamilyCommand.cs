90b1a8e521261fc9b55df1b1a4ce06d4bf6c1c87
Blowa <blowaxd693@gmail.com>
2017-12-06 15:07:19 +0100

    add some documentation and cleanup UpdateFamily Command

--- srcs/NosSharp.CLI/Commands/UpdateFamilyCommand.cs
+++ srcs/NosSharp.CLI/Commands/UpdateFamilyCommand.cs
@@ -4,22 +4,24 @@ namespace NosSharp.CLI.Commands
 {
     class UpdateFamilyCommand : AbstractCliCommand
     {
+        private readonly long _familyId;
+        private readonly bool _isFactionChange;
+        
         /// <summary>
-        /// 
+        /// Instanciate an UpdateFamilyCommand
         /// </summary>
-        public bool IsFactionChange { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public UpdateFamilyCommand() : base("FamilyUpdate")
+        /// <param name="familyId">FamilyId that has to be updated</param>
+        /// <param name="isFactionChange">Is the Family actually changing its faction</param>
+        public UpdateFamilyCommand(long familyId, bool isFactionChange) : base("FamilyUpdate")
         {
+            _familyId = familyId;
+            _isFactionChange = isFactionChange;
         }
 
         /// <inheritdoc />
         public override string GetContent()
         {
-            return $"{Content} {IsFactionChange}";
+            return $"{_familyId} {_isFactionChange}";
         }
     }
 }
--- srcs/NosSharp.CLI/Interfaces/AbstractCLICommand.cs
+++ srcs/NosSharp.CLI/Interfaces/AbstractCLICommand.cs
@@ -3,26 +3,19 @@
     public abstract class AbstractCliCommand : ICliCommand
     {
         private readonly string _header;
-        protected string Content;
 
         protected AbstractCliCommand(string header)
         {
             _header = header;
         }
         
-        /// <summary>
-        /// Get Header of <see cref="ICliCommand"/>
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc />
         public string GetHeader()
         {
             return _header;
         }
 
-        /// <summary>
-        /// Get Content of <see cref="ICliCommand"/>
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc />
         public abstract string GetContent();
     }
 }
