20ddfddc21373eaa98cf691769f3b74fcb93ef79
Kraken <kraken.kk1@gmail.com>
2017-12-08 21:28:03 +0100

    Fix possible nre

--- srcs/OpenNos.Handler/NpcPacketHandler.cs
+++ srcs/OpenNos.Handler/NpcPacketHandler.cs
@@ -878,7 +878,7 @@ namespace OpenNos.Handler
                     return;
                 }
 
-                TeleporterDTO tp = npc.Teleporters?.FirstOrDefault(t => t.Type == TeleporterType.TeleporterOnMap);
+                TeleporterDTO tp = npc.Teleporters?.FirstOrDefault(t => t?.Type == TeleporterType.TeleporterOnMap);
                 if (tp != null)
                 {
                     Session.SendPacket(UserInterfaceHelper.Instance.GenerateDelay(5000, 1, $"#guri^710^{tp.MapY}^{tp.MapX}^{npc.MapNpcId}"));
