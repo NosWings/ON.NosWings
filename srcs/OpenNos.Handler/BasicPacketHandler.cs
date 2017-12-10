e1834e97e251a220880db676bc4e14fd82c3b258
Elendan <arthur.despeaux@hotmail.fr>
2017-12-11 00:31:14 +0100

    fix previous commit

--- srcs/OpenNos.Handler/BasicPacketHandler.cs
+++ srcs/OpenNos.Handler/BasicPacketHandler.cs
@@ -618,7 +618,7 @@ namespace OpenNos.Handler
         {
             double currentRunningSeconds = (DateTime.Now - Process.GetCurrentProcess().StartTime.AddSeconds(-50)).TotalSeconds;
             double timeSpanSinceLastPortal = currentRunningSeconds - Session.Character.LastPortal;
-            if (!(timeSpanSinceLastPortal >= 4) || !Session.HasCurrentMapInstance)
+            if (!(timeSpanSinceLastPortal >= 4) && Session.CurrentMapInstance?.MapInstanceType != MapInstanceType.RaidInstance || !Session.HasCurrentMapInstance)
             {
                 Session.SendPacket(Session.Character.GenerateSay(Language.Instance.GetMessageFromKey("CANT_MOVE"), 10));
                 return;
