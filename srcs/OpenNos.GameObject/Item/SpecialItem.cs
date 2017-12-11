57c265c6b6235f67ec4d59baecfaaf3b48a995cc
Elendan <arthur.despeaux@hotmail.fr>
2017-12-11 22:41:38 +0100

    fix teleport

--- srcs/OpenNos.GameObject/Item/SpecialItem.cs
+++ srcs/OpenNos.GameObject/Item/SpecialItem.cs
@@ -327,7 +327,7 @@ namespace OpenNos.GameObject.Item
                             break;
 
                     }
-                    Observable.Timer(TimeSpan.FromSeconds(session.Character.BuffRandomTime)).Subscribe(o =>
+                    Observable.Timer(TimeSpan.FromMilliseconds(session.Character.BuffRandomTime)).Subscribe(o =>
                     {
                         session.Character.Speed -= 5;
                         switch (session.Character.Morph)
