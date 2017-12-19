27606252b257b20f321799da61465f99acc8bfd1
Kraken <kraken.kk1@gmail.com>
2017-12-19 22:14:36 +0100

    Add Aphrodite100

---
+++ srcs/OpenNos.DAL.EF/Migrations/201712192113598_Aphrodite100.cs
@@ -0,0 +1,63 @@
+namespace OpenNos.DAL.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Aphrodite100 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.QuestObjective",
+                c => new
+                    {
+                        QuestObjectiveId = c.Int(nullable: false, identity: true),
+                        QuestId = c.Int(nullable: false),
+                        Data = c.Int(),
+                        Objective = c.Int(),
+                        SpecialData = c.Int(),
+                        DropRate = c.Int(),
+                        ObjectiveIndex = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.QuestObjectiveId);
+            
+            DropColumn("dbo.Quest", "FirstData");
+            DropColumn("dbo.Quest", "FirstObjective");
+            DropColumn("dbo.Quest", "FirstSpecialData");
+            DropColumn("dbo.Quest", "SecondData");
+            DropColumn("dbo.Quest", "SecondObjective");
+            DropColumn("dbo.Quest", "SecondSpecialData");
+            DropColumn("dbo.Quest", "ThirdData");
+            DropColumn("dbo.Quest", "ThirdObjective");
+            DropColumn("dbo.Quest", "ThirdSpecialData");
+            DropColumn("dbo.Quest", "FourthData");
+            DropColumn("dbo.Quest", "FourthObjective");
+            DropColumn("dbo.Quest", "FourthSpecialData");
+            DropColumn("dbo.Quest", "FifthData");
+            DropColumn("dbo.Quest", "FifthObjective");
+            DropColumn("dbo.Quest", "FifthSpecialData");
+            DropColumn("dbo.Quest", "SpecialData");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Quest", "SpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "FifthSpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "FifthObjective", c => c.Int());
+            AddColumn("dbo.Quest", "FifthData", c => c.Int());
+            AddColumn("dbo.Quest", "FourthSpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "FourthObjective", c => c.Int());
+            AddColumn("dbo.Quest", "FourthData", c => c.Int());
+            AddColumn("dbo.Quest", "ThirdSpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "ThirdObjective", c => c.Int());
+            AddColumn("dbo.Quest", "ThirdData", c => c.Int());
+            AddColumn("dbo.Quest", "SecondSpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "SecondObjective", c => c.Int());
+            AddColumn("dbo.Quest", "SecondData", c => c.Int());
+            AddColumn("dbo.Quest", "FirstSpecialData", c => c.Int());
+            AddColumn("dbo.Quest", "FirstObjective", c => c.Int(nullable: false));
+            AddColumn("dbo.Quest", "FirstData", c => c.Int(nullable: false));
+            DropTable("dbo.QuestObjective");
+        }
+    }
+}
